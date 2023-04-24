5685d26044de6f48cffdf5d70edfbf96e7957654
Guibrick <guido.bertaina@appliedtechnology.se>
2023-04-24 12:20:25 +0200

    Add Database.

---
+++ Backend/Data/JuridikContext.cs
@@ -0,0 +1,22 @@
+using JuridikApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuridikApp.Data
+{
+    public class JuridikContext : DbContext
+    {
+        public JuridikContext(DbContextOptions<JuridikContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Query> Queries { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("JuridikContext"));
+        }
+    }
+}
---
+++ Backend/Migrations/20230424100549_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JuridikApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Queries",
+                columns: table => new
+                {
+                    QueryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CaseDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CasePlace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CaseClaimant = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CaseDefendant = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicableLaw = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicableJurisprudence = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Judgement = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Queries", x => x.QueryId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Queries");
+        }
+    }
+}
